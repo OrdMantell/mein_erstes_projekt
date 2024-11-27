53cca5eb719f9f6ee4eb55a56062d64c6ff42132
OrdMantell <thmnk822@gmail.com>
2024-11-27 09:48:18 +0100

    Aufgaben3 updated

--- aufg3.py
+++ aufg3.py
@@ -1,4 +1,4 @@
-
+'''
 # Aufgabe 1
 xy = 1
 while True:
@@ -32,3 +32,59 @@ while True:
         print("Falsches Passwort! Versuche es erneut!")
 
 # Aufgabe 4
+for i in range(1, 11):
+    print(i)
+
+# Aufgabe 5a
+for gerade in range(1, 21):
+    if gerade % 2 == 0:
+        print(gerade)
+
+# Aufgabe 5b
+zliste = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+for f in range(4, len(zliste), 5):
+    print(zliste[f])
+'''
+# Aufgabe 6
+vertik = 5
+horiz = 5
+for r in range(vertik):
+    print("*" * horiz)
+
+# Überlegung zu 6: Rechteck Umrandung
+vertik = 7
+horiz = 6
+for r in range(horiz):
+    if r == 0 or r == horiz - 1: # erste/letzte Reihe alles Sterne
+        print("* " * vertik)
+    else:
+        print("* " + "  " * (vertik - 2) + "* ") # nur erste/letze Spalte Sterne
+
+# Aufgabe 7
+for backw in range(10, 0, -1):
+    print(backw)
+
+# Aufgabe 8
+lnamen = ["Anna", "Max", "Tom", "Lisa"]
+print("Es sind", len(lnamen), "Namen in der Liste:", (lnamen))
+lnamen.append("Marie")
+print("Es sind", len(lnamen), "Namen in der Liste:", (lnamen))
+lnamen.remove("Tom")
+print("Es sind", len(lnamen), "Namen in der Liste:", (lnamen))
+
+# Aufgabe 9
+lnummern = [5, 3, 8, 1, 2]
+lnummern.reverse()
+print("rückwärts:", lnummern) # muss VOR sort passieren
+lnummern.sort()
+print("aufsteigend:", lnummern)
+lnummern.sort(reverse=True)
+print("absteigend:", lnummern)
+
+# Aufgabe 10
+lzahlen = [1, 2, 3, 4, 5]
+lzahlen.append(6)
+lzahlen.insert(0, 0)
+tres = lzahlen.count(3)
+print("Die Liste:", lzahlen, "enthält", len(lzahlen), "Einträge.")
+print("Die Zahl drei kommt", tres, "mal vor.")
